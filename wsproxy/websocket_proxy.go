0ac28561c14aa840b22c64653586e06ec4985e7d
Travis Cline <travis.cline@gmail.com>
2017-04-18 14:11:15 -0700

    Customizability (#6)
    * add options to customize behavior
    
    consider networked closed errors as ok
    
    * use returned value from RequestMutatorFunc
    
    * include deprecation warning on both

--- wsproxy/websocket_proxy.go
+++ wsproxy/websocket_proxy.go
@@ -6,15 +6,77 @@ import (
 	"net/http"
 	"strings"
 
-	log "github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/websocket"
 	"golang.org/x/net/context"
 )
 
-var (
-	MethodOverrideParam = "method"
-	TokenCookieName     = "token"
-)
+// MethodOverrideParam defines the special URL parameter that is translated into the subsequent proxied streaming http request's method.
+//
+// Deprecated: it is preferable to use the Options parameters to WebSocketProxy to supply parameters.
+var MethodOverrideParam = "method"
+
+// TokenCookieName defines the cookie name that is translated to an 'Authorization: Bearer' header in the streaming http request's headers.
+//
+// Deprecated: it is preferable to use the Options parameters to WebSocketProxy to supply parameters.
+var TokenCookieName = "token"
+
+// RequestMutatorFunc can supply an alternate outgoing request.
+type RequestMutatorFunc func(incoming *http.Request, outgoing *http.Request) *http.Request
+
+// Proxy provides websocket transport upgrade to compatible endpoints.
+type Proxy struct {
+	h                   http.Handler
+	logger              Logger
+	methodOverrideParam string
+	tokenCookieName     string
+	requestMutator      RequestMutatorFunc
+}
+
+// Logger collects log messages.
+type Logger interface {
+	Warnln(...interface{})
+	Debugln(...interface{})
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !websocket.IsWebSocketUpgrade(r) {
+		p.h.ServeHTTP(w, r)
+		return
+	}
+	p.proxy(w, r)
+}
+
+// Option allows customization of the proxy.
+type Option func(*Proxy)
+
+// WithMethodParamOverride allows specification of the special http parameter that is used in the proxied streaming request.
+func WithMethodParamOverride(param string) Option {
+	return func(p *Proxy) {
+		p.methodOverrideParam = param
+	}
+}
+
+// WithTokenCookieName allows specification of the cookie that is supplied as an upstream 'Authorization: Bearer' http header.
+func WithTokenCookieName(param string) Option {
+	return func(p *Proxy) {
+		p.methodOverrideParam = param
+	}
+}
+
+// WithRequestMutator allows a custom RequestMutatorFunc to be supplied.
+func WithRequestMutator(fn RequestMutatorFunc) Option {
+	return func(p *Proxy) {
+		p.requestMutator = fn
+	}
+}
+
+// WithLogger allows a custom FieldLogger to be supplied
+func WithLogger(logger Logger) Option {
+	return func(p *Proxy) {
+		p.logger = logger
+	}
+}
 
 // WebsocketProxy attempts to expose the underlying handler as a bidi websocket stream with newline-delimited
 // JSON as the content encoding.
@@ -28,25 +90,38 @@ var (
 //   Authorization: Bearer foobar
 //
 // Method can be overwritten with the MethodOverrideParam get parameter in the requested URL
-func WebsocketProxy(h http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if !websocket.IsWebSocketUpgrade(r) {
-			h.ServeHTTP(w, r)
-			return
-		}
-		websocketProxy(w, r, h)
+func WebsocketProxy(h http.Handler, opts ...Option) http.Handler {
+	p := &Proxy{
+		h:                   h,
+		logger:              logrus.New(),
+		methodOverrideParam: MethodOverrideParam,
+		tokenCookieName:     TokenCookieName,
 	}
+	for _, o := range opts {
+		o(p)
+	}
+	return p
 }
 
+// TODO(tmc): allow modification of upgrader settings?
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
+func isClosedConnError(err error) bool {
+	str := err.Error()
+	if strings.Contains(str, "use of closed network connection") {
+		return true
+	}
+	return websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway)
+}
+
+func (p *Proxy) proxy(w http.ResponseWriter, r *http.Request) {
 	var responseHeader http.Header
 	// If Sec-WebSocket-Protocol starts with "Bearer", respond in kind.
+	// TODO(tmc): consider customizability/extension point here.
 	if strings.HasPrefix(r.Header.Get("Sec-WebSocket-Protocol"), "Bearer") {
 		responseHeader = http.Header{
 			"Sec-WebSocket-Protocol": []string{"Bearer"},
@@ -54,7 +129,7 @@ func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	}
 	conn, err := upgrader.Upgrade(w, r, responseHeader)
 	if err != nil {
-		log.Warnln("error upgrading websocket:", err)
+		p.logger.Warnln("error upgrading websocket:", err)
 		return
 	}
 	defer conn.Close()
@@ -65,24 +140,28 @@ func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	requestBodyR, requestBodyW := io.Pipe()
 	request, err := http.NewRequest(r.Method, r.URL.String(), requestBodyR)
 	if err != nil {
-		log.Warnln("error preparing request:", err)
+		p.logger.Warnln("error preparing request:", err)
 		return
 	}
 	if swsp := r.Header.Get("Sec-WebSocket-Protocol"); swsp != "" {
 		request.Header.Set("Authorization", strings.Replace(swsp, "Bearer, ", "Bearer ", 1))
 	}
 	// If token cookie is present, populate Authorization header from the cookie instead.
-	if cookie, err := r.Cookie(TokenCookieName); err == nil {
+	if cookie, err := r.Cookie(p.tokenCookieName); err == nil {
 		request.Header.Set("Authorization", "Bearer "+cookie.Value)
 	}
-	if m := r.URL.Query().Get(MethodOverrideParam); m != "" {
+	if m := r.URL.Query().Get(p.methodOverrideParam); m != "" {
 		request.Method = m
+	}
+
+	if p.requestMutator != nil {
+		request = p.requestMutator(r, request)
 	}
 
 	responseBodyR, responseBodyW := io.Pipe()
 	go func() {
 		<-ctx.Done()
-		log.Debugln("closing pipes")
+		p.logger.Debugln("closing pipes")
 		requestBodyW.CloseWithError(io.EOF)
 		responseBodyW.CloseWithError(io.EOF)
 	}()
@@ -90,7 +169,7 @@ func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	response := newInMemoryResponseWriter(responseBodyW)
 	go func() {
 		defer cancelFn()
-		h.ServeHTTP(response, request)
+		p.h.ServeHTTP(response, request)
 	}()
 
 	// read loop -- take messages from websocket and write to http request
@@ -101,27 +180,27 @@ func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
 		for {
 			select {
 			case <-ctx.Done():
-				log.Debugln("read loop done")
+				p.logger.Debugln("read loop done")
 				return
 			default:
 			}
-			log.Debugln("[read] reading from socket.")
-			_, p, err := conn.ReadMessage()
+			p.logger.Debugln("[read] reading from socket.")
+			_, payload, err := conn.ReadMessage()
 			if err != nil {
-				if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
-					log.Debugln("[read] websocket closed successfully:", err)
+				if isClosedConnError(err) {
+					p.logger.Debugln("[read] websocket closed:", err)
 					return
 				}
-				log.Warnln("error reading websocket message:", err)
+				p.logger.Warnln("error reading websocket message:", err)
 				return
 			}
-			log.Debugln("[read] read payload:", string(p))
-			log.Debugln("[read] writing to requestBody:")
-			n, err := requestBodyW.Write(p)
+			p.logger.Debugln("[read] read payload:", string(payload))
+			p.logger.Debugln("[read] writing to requestBody:")
+			n, err := requestBodyW.Write(payload)
 			requestBodyW.Write([]byte("\n"))
-			log.Debugln("[read] wrote to requestBody", n)
+			p.logger.Debugln("[read] wrote to requestBody", n)
 			if err != nil {
-				log.Warnln("[read] error writing message to upstream http server:", err)
+				p.logger.Warnln("[read] error writing message to upstream http server:", err)
 				return
 			}
 		}
@@ -130,17 +209,17 @@ func websocketProxy(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	scanner := bufio.NewScanner(responseBodyR)
 	for scanner.Scan() {
 		if len(scanner.Bytes()) == 0 {
-			log.Warnln("[write] empty scan", scanner.Err())
+			p.logger.Warnln("[write] empty scan", scanner.Err())
 			continue
 		}
-		log.Debugln("[write] scanned", scanner.Text())
+		p.logger.Debugln("[write] scanned", scanner.Text())
 		if err = conn.WriteMessage(websocket.TextMessage, scanner.Bytes()); err != nil {
-			log.Warnln("[write] error writing websocket message:", err)
+			p.logger.Warnln("[write] error writing websocket message:", err)
 			return
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		log.Warnln("scanner err:", err)
+		p.logger.Warnln("scanner err:", err)
 	}
 }
 
